38bcd2b42cff48d09b8c88dc51ba576bf928923a
Stephen Bussey <sb8244@cs.ship.edu>
2014-02-01 17:30:26 -0500

    Email validation

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -1,3 +1,6 @@
 class Professor < ActiveRecord::Base
-  validates( :name, presence: true )
+  validates :name, presence: true 
+  validates :email, presence: true, 
+            :email_format => {:message => 'is not an email'},
+            uniqueness: { case_sensitive: false }
 end
--- spec/models/professor_spec.rb
+++ spec/models/professor_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe Professor do
+
   before {
     @professor = Professor.new(
       name: "Carol Wellington",
@@ -10,11 +11,33 @@ describe Professor do
 
   subject { @professor }
 
+  #Tests for our fields existing
   it { should respond_to(:name) }
   it { should respond_to(:email) }
 
   describe "when name is not present" do
     before { @professor.name = " " }
     it { should_not be_valid }
+  end
+
+  describe "when email has invalid format" do
+    it "should be invalid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+                     foo@bar_baz.com foo@bar+baz.com]
+      addresses.each do |invalid_address|
+        @professor.email = invalid_address
+        expect(@professor).not_to be_valid
+      end
+    end
+  end
+
+  describe "when email has valid format" do
+    it "should be valid" do
+      addresses = %w[test@test.com]
+      addresses.each do |valid_address|
+        @professor.email = valid_address
+        expect(@professor).to be_valid
+      end
+    end
   end
 end
