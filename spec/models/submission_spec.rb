3e0b4ef235b30f7591facc038df5513ee39cbb82
Stephen Bussey <sb8244@cs.ship.edu>
2014-02-01 17:54:53 -0500

    Submission test base

--- spec/models/submission_spec.rb
+++ spec/models/submission_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe Submission do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before {
+    @submission = Submission.new(
+      student_name: "Student",
+      student_email: "student@ship.edu",
+      course_number: "CSC411",
+      start_time: DateTime.now,
+      class_length: 50,
+      exam_pickup: "Student",
+      exam_return: "Student",
+      reader: false,
+      laptop: false,
+      scribe: false
+    )
+  }
+
+  subject { @submission }
+
+  it { should be_valid }
 end
