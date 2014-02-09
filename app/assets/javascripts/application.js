1344600eba11045c46a27f90773ab41777149893
Stephen Bussey <sb8244@cs.ship.edu>
2014-02-08 20:35:48 -0500

    Typeahead added for professor on submission

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,17 +18,40 @@
 
 (function($) {
   $(document).ready(function() {
-    $("#submission_professor_name").keyup(function() {
-      var val = $(this).val();
-      $.get("/professors/list?name=" + val, function(res) {
-        console.log(res);
+    var nameSource = function(query, cb) {
+      $.get('/professors/list?name=' + query, function(data) {
+        cb(processData(data));
       });
-    });
-    $("#submission_professor_email").keyup(function() {
-      var val = $(this).val();
-      $.get("/professors/list?email=" + val, function(res) {
-        console.log(res);
+    };
+    var emailSource = function(query, cb) {
+      $.get('/professors/list?email=' + query, function(data) {
+        cb(processData(data));
       });
+    };
+
+    $("#submission_professor_name").typeahead(null,{
+      source: nameSource,
+      displayKey: 'name',
+    }).on('typeahead:selected', function (obj, datum) {
+      $("#submission_professor_email").typeahead('val', datum.email).typeahead('close');
     });
+
+    $("#submission_professor_email").typeahead(null,{
+      source: emailSource,
+      displayKey: 'email',
+    }).on('typeahead:selected', function (obj, datum) {
+      $("#submission_professor_name").typeahead('val', datum.name).typeahead('close');
+    });
+
+    function processData(data) {
+      var names = [];
+      for(var i = 0; i < data.length; i++) {
+        names.push({
+          email: data[i].email,
+          name: data[i].name
+        });
+      }
+      return names;
+    }
   });
 })(jQuery);
