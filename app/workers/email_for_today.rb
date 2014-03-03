4b1b988ee2bbe7c1209e8b89556b139cc9e2bd5c
Stephen Bussey <sb8244@cs.ship.edu>
2014-03-03 16:12:13 -0500

    Add sidetiq worker

--- app/workers/email_for_today.rb
+++ app/workers/email_for_today.rb
@@ -2,6 +2,8 @@ class EmailForToday
   include Sidekiq::Worker
   include Sidetiq::Schedulable
 
+  recurrence { daily.hour_of_day(6) }
+
   def perform
     submissions = Submission.today
     SubmissionsMailer.today(submissions).deliver
