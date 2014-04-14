db8e2472b5ee215321421769e2bef5daa38b3b27
Stephen Bussey <sb8244@cs.ship.edu>
2014-04-14 15:50:15 -0400

    Send a confirmation email upon rescheduling

--- spec/controllers/submissions_controller_spec.rb
+++ spec/controllers/submissions_controller_spec.rb
@@ -144,6 +144,12 @@ describe SubmissionsController do
       post :reschedule, id: submission.id
       expect(response).to redirect_to(Submission.last)
     end
+
+    it "sends a mail" do
+      expect {
+        post :reschedule, id: submission.id
+      }.to change{ ActionMailer::Base.deliveries.count }.by(1)
+    end
   end
 
   describe 'POST :create' do
