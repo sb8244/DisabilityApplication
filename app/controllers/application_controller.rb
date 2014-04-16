ac8e4d77415ffbb9553966ee07726736a3879ee3
Stephen Bussey <sb8244@cs.ship.edu>
2014-04-15 23:50:28 -0400

    always do auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  unless ENV["HTTP_AUTH_NAME"].blank? || ENV["HTTP_AUTH_PASSWORD"].blank?
-    http_basic_authenticate_with name: ENV["HTTP_AUTH_NAME"], password: ENV["HTTP_AUTH_PASSWORD"]
-  end
+  http_basic_authenticate_with name: ENV["HTTP_AUTH_NAME"], password: ENV["HTTP_AUTH_PASSWORD"]
 end
