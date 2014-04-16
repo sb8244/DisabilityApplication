949517907fc84d658fa702b3627af9f1c624448c
Stephen Bussey <sb8244@cs.ship.edu>
2014-04-15 21:20:57 -0400

    Conditionally do auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  http_basic_authenticate_with name: ENV["HTTP_AUTH_NAME"], password: ENV["HTTP_AUTH_PASSWORD"]
+
+  unless ENV["HTTP_AUTH_NAME"].blank? || ENV["HTTP_AUTH_PASSWORD"].blank?
+    http_basic_authenticate_with name: ENV["HTTP_AUTH_NAME"], password: ENV["HTTP_AUTH_PASSWORD"]
+  end
 end
