d15f6f4330a376489a751a7f523ba75a55a4df18
Stephen Bussey <steve.bussey@salesloft.com>
2016-04-25 22:28:06 -0400

    try auto tls

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,7 +1,8 @@
 DisabilityApp::Application.configure do
   ActionMailer::Base.smtp_settings = {
-    :address   => ENV.fetch("SMTP_ADDRESS"),
-    :port      => ENV.fetch("SMTP_PORT", 25),
-    :domain => 'ada.cs.ship.edu' # your domain to identify your server when connecting
+    address: ENV.fetch("SMTP_ADDRESS"),
+    port: ENV.fetch("SMTP_PORT", 25),
+    enable_starttls_auto: true,
+    domain: 'ada.cs.ship.edu' # your domain to identify your server when connecting
   }
 end
