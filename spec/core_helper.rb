b7923ea58d981ecd39b6e692be92ad5ae022f3ed
Stephen Bussey <steve.bussey@salesloft.com>
2017-03-12 22:03:05 -0400

    safer paths (no / \ discrepancy)

--- spec/core_helper.rb
+++ spec/core_helper.rb
@@ -1,7 +1,7 @@
 require_relative "../core/load"
 require 'rspec/autorun'
 
-Dir[File.dirname(__FILE__) + '/core/factories/*.rb'].each {|file| require file }
+Dir[File.join(File.dirname(__FILE__), 'core', 'factories', '*.rb')].each {|file| require file }
 
 RSpec.configure do |config|
   config.order = "random"
