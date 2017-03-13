7ed30e56c00df0bcfaec665f4e1e5250dce99f77
Stephen Bussey <steve.bussey@salesloft.com>
2017-03-12 21:47:21 -0400

    get the modules out of the base folder

--- core/load.rb
+++ core/load.rb
@@ -3,8 +3,9 @@ require "active_model"
 
 require_relative "./validator"
 
-require_relative "./models/attribute_assignment"
-require_relative "./models/validatable"
+require_relative "./models/mixins/attribute_assignment"
+require_relative "./models/mixins/validatable"
+
 require_relative "./models/professor"
 require_relative "./models/submission"
 
