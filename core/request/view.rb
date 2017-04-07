65ea852ddbd9621419c2dabf0034566bbb1a0b5f
Stephen Bussey <sb8244@cs.ship.edu>
2017-04-07 14:12:52 -0400

    No reason for this view right now

--- core/request/view.rb
+++
@@ -1,9 +0,0 @@
-class View
-  def initialize(attributes = {})
-    @attributes = attributes
-  end
-
-  def method_missing(method, *args)
-    @attributes[method] unless method.to_s.include?('=')
-  end
-end
--- core/requests/professor/list.rb
+++ core/requests/professor/list.rb
@@ -2,6 +2,6 @@ class ProfessorListRequest < Request
   def request
     authorized!
     professors = ProfessorRepository.all(like_name: params[:name], like_email: params[:email])
-    response.view = View.new(professors: professors)
+    response.view = OpenStruct.new(professors: professors)
   end
 end
